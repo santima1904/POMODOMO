692edfd55aaf7cc95938a3587e476af65a6cffd8
Santi <santima1904@gmail.com>
2021-10-05 09:20:12 +0200

    Ejercicio 1.3.2

--- Boletin3.2/Ejercicio1/app/src/main/java/es/iesnervion/smartinez/ejercicio132/MainActivity.java
+++ Boletin3.2/Ejercicio1/app/src/main/java/es/iesnervion/smartinez/ejercicio132/MainActivity.java
@@ -2,13 +2,77 @@ package es.iesnervion.smartinez.ejercicio132;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.util.TypedValue;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        Button boton;
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        boton = findViewById(R.id.boton);
+
+        boton.setOnClickListener(this);
+
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        TextView text = findViewById(R.id.text);
+
+        CheckBox bold;
+        CheckBox gigant;
+        CheckBox tiny;
+        CheckBox red;
+
+        bold = findViewById(R.id.bold);
+        gigant = findViewById(R.id.gigant);
+        tiny = findViewById(R.id.tiny);
+        red = findViewById(R.id.red);
+
+       if (bold.isChecked()){
+
+           text.setTypeface(Typeface.DEFAULT_BOLD);
+       }else{
+
+           text.setTypeface(Typeface.DEFAULT);
+       }
+
+       if (gigant.isChecked()){
+
+           text.setTextSize(TypedValue.COMPLEX_UNIT_PX, 300);
+       }else{
+
+           text.setTextSize(TypedValue.COMPLEX_UNIT_PX, 45);
+       }
+
+        if (tiny.isChecked()){
+
+            text.setTextSize(TypedValue.COMPLEX_UNIT_PX, 20);
+        }else{
+
+            text.setTextSize(TypedValue.COMPLEX_UNIT_PX, 45);
+        }
+
+        if (red.isChecked()){
+
+            text.setTextColor(Color.RED);
+        }else{
+
+            text.setTextColor(Color.CYAN);
+        }
+
     }
 }
