5f3c6b7fa192578d6eaeeddaa2011451d9881d2a
Santi <santima1904@gmail.com>
2021-11-02 11:07:53 +0100

    spinner hechos

--- PruebaSpinner/app/src/main/java/iesnervion/smartinez/pruebaspinner/MainActivity.java
+++ PruebaSpinner/app/src/main/java/iesnervion/smartinez/pruebaspinner/MainActivity.java
@@ -4,15 +4,25 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.text.Layout;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.Spinner;
+import android.widget.SpinnerAdapter;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
-    private List<String> verduras = new ArrayList<>();
+    Verdura [] verduras = {new Verdura("Berenjena", R.drawable.berenjena), new Verdura("Cebolla", R.drawable.cebolla),
+            new Verdura("Puerro", R.drawable.puerro), new Verdura("Patata", R.drawable.patata)};
     private Spinner spinner;
 
     @Override
@@ -20,14 +30,139 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        verduras.add("Berenjena");
-        verduras.add("Cebolla");
-        verduras.add("Patata");
-        verduras.add("Puerro");
-
         spinner = findViewById(R.id.spinner);
+        IconicAdapter<Verdura> adapter = new IconicAdapter<>(this, verduras);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
 
-        spinner.setAdapter(new ArrayAdapter<String>());
+    }
 
-        
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+        TextView text = findViewById(R.id.name);
+        text.setText(verduras[position].getNombre());
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
+
+    class IconicAdapter<T> extends BaseAdapter implements SpinnerAdapter {
+
+        Context contextAdapter;
+        T[] lista;
+
+        //Constructor
+        public IconicAdapter(Context context, T[] listaDatos) {
+            contextAdapter = context;
+            lista = listaDatos;
+        }
+
+
+        @Override
+        public View getDropDownView(int position, View convertView, ViewGroup parent) {
+
+            ViewHolder2 holder;
+            View row = convertView;
+            TextView text;
+
+            if (row == null){
+                LayoutInflater inflater = getLayoutInflater();
+
+                row = inflater.inflate(R.layout.spinner_lista, parent, false);
+
+                text = (TextView) row.findViewById(R.id.texto);
+
+                holder = new ViewHolder2(text);
+                row.setTag(holder);
+            }else{
+                holder = (ViewHolder2) row.getTag();
+            }
+
+            holder.getText().setText(verduras[position].getNombre());
+
+            return row;
+        }
+
+
+
+        @Override
+        public int getCount() {
+            return verduras.length;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return verduras[position];
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+
+            ViewHolder holder;
+            View row = convertView;
+            TextView text;
+            ImageView image;
+
+            if (row == null){
+                LayoutInflater inflater = getLayoutInflater();
+
+                    row = inflater.inflate(R.layout.spinner_seleccionado, parent, false);
+
+                    text = (TextView) row.findViewById(R.id.texto_seleccionado);
+                    image = (ImageView) row.findViewById(R.id.img);
+
+
+                holder = new ViewHolder(image, text);
+                row.setTag(holder);
+            }else{
+                holder = (ViewHolder) row.getTag();
+            }
+
+            holder.getText().setText(verduras[position].getNombre());
+            holder.getImage().setImageResource(verduras[position].getImage());
+
+            return row;
+        }
+
+    }
+
+    class ViewHolder{
+        TextView text;
+        ImageView image;
+
+        public ViewHolder(ImageView image, TextView text) {
+            this.text = text;
+            this.image = image;
+        }
+
+        public TextView getText() {
+            return text;
+        }
+
+        public ImageView getImage() {
+            return image;
+        }
+    }
+
+    class ViewHolder2{
+        TextView text;
+
+        public ViewHolder2(TextView text) {
+            this.text = text;
+        }
+
+        public TextView getText() {
+            return text;
+        }
+
+    }
+
 }
